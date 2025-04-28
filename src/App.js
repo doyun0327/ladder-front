340e0474b2ea74d525a556c18351bb1c317fba34
doyun <idoyun781@gmail.com>
2025-04-28 13:57:58 +0900

    feat : roomid생성 및 당첨레일 반환 api 요청`

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SseProvider } from './context/SseContext';
-// import Home from './components/Home';
-import GameRoom from './components/GameRoom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SseProvider } from "./context/SseContext";
+import Home from "./components/Home";
+import GameRoom from "./components/GameRoom";
 import LadderBoard from "./components/LadderBoard";
-import './App.css';
+import "./App.css";
 function App() {
-    return (
-        <SseProvider>
-            <Router>
-                <Routes>
-                    {/*<Route path="/" element={<Home />} />*/}
-                    <Route path="/" element={<LadderBoard />} />
-                    <Route path="/game/:roomId" element={<GameRoom />} />
-                </Routes>
-            </Router>
-        </SseProvider>
-    );
+  return (
+    <SseProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<LadderBoard />} /> */}
+          <Route path="/game/:roomId" element={<GameRoom />} />
+        </Routes>
+      </Router>
+    </SseProvider>
+  );
 }
 
 export default App;
